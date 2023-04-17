c7ddba8407a40d2f85e0d976a931eeaa5d4bf582
Kung Lo <professionally_unemployed@proton.me>
2023-04-17 11:19:42 +0000

    Passed

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,29 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        HashMap<Character, Integer> inputStr = new HashMap<>();
+        int maxValue = 0;
+        char mostChar = ' '; 
+        
+        for(char c : str.toCharArray())
+        {           
+            if(!inputStr.containsKey(c))
+            {
+                inputStr.put(c, 0);
+                continue;
+            }
+            inputStr.replace(c, inputStr.get(c)+1);
+        }
+
+        for(Entry<Character, Integer> set: inputStr.entrySet())
+        {
+            if(set.getValue() > maxValue)
+            {
+                maxValue = set.getValue();
+                mostChar = set.getKey();
+            }
+        }
+        
+        return mostChar;
     }
 }
